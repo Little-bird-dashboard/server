ebc41d34adcabd1212f8001d786782cf0ae0fbdb
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-08-04 15:36:55 -0600

    logging req.body

--- routes/sms.js
+++ routes/sms.js
@@ -7,7 +7,7 @@ Router.post('/', (req, res) => {
   var twilio = require('twilio');
   var twiml = new twilio.twiml.MessagingResponse();
   twiml.message('The Robots are coming! Head for the hills!');
-  console.log(req.body.Body);
+  console.log(req.body);
   Queries.insertOneCommunication()
     .then(communication=> {
       res.send(communication);
