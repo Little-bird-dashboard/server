f670871d3b2a883f2b29e3de96d1833d86bca35e
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-08-07 11:30:08 -0600

    routes working for accepting responses and ad hoc texts from teacher

--- database/smsqueries.js
+++ database/smsqueries.js
@@ -3,5 +3,20 @@ const knex = require('./knex');
 module.exports = {
   insertOneCommunication: (message) => {
     return knex('communication').insert(message).returning('*');
+  },
+  findStudentandStakeholder: (cell) => {
+    return knex('stakeholder AS a')
+    .select('a.id AS stakeholder_id', 'b.student_id AS student_id')
+    .innerJoin('student_stakeholder AS b', 'b.stakeholder_id', 'a.id')
+    .where('a.cell', cell)
+    .first();
+  },
+  findGuardianCellById: (studentID) => {
+    return knex('student_stakeholder AS a')
+    .select('b.cell', 'b.id AS stakeholder_id')
+    .innerJoin('stakeholder AS b', 'a.stakeholder_id', 'b.id')
+    .where('a.student_id', studentID)
+    .andWhere('b.stakeholder_type_id', 2)
+    .first();
   }
 };
--- routes/sms.js
+++ routes/sms.js
@@ -1,6 +1,40 @@
 const Express = require('express');
 const Router = Express.Router();
 const Queries = require('../database/smsqueries');
+require('dotenv').config();
+var client = require('twilio')(process.env.accountSid, process.env.authToken);
+
+Router.post('/single/:id', (req,res) => {
+  Queries.findGuardianCellById(req.params.id)
+    .then(response=>{
+      client.messages.create({
+          to: response.cell,
+          from: '+18042982615',
+          body: req.body.message,
+      }, function(err, message) {
+          console.log(message.sid);
+          message_info= {
+            communication_type_id: 1,
+            raw_body: message.body,
+            timestamp: Date.now(),
+            student_id: req.params.id,
+            stakeholder_id: response.guardian_id,
+            MessageSid: message.sid,
+            AccountSid: message.AccountSid,
+            message_status: message.status,
+            communication_recipient_contact: message.to,
+            communication_sender_contact: message.from
+          };
+          Queries.insertOneCommunication(message_info)
+            .then(communication => {
+              console.log(communication);
+              res.send(communication);
+            });
+      });
+
+    });
+  //insert into db
+});
 
 Router.post('/', (req, res) => {
   console.log('text sent');
@@ -8,29 +42,29 @@ Router.post('/', (req, res) => {
   var twiml = new twilio.twiml.MessagingResponse();
   twiml.message('The Robots are coming! Head for the hills!');
   console.log(req.body);
-  //make object of what to keep in database
-  message= {
-    communication_type_id: 1,
-    raw_body: req.body.Body,
-    timestamp: Date.now(),
-    student_id: 1 ,
-    stakeholder_id: 2,
-    MessageSid: req.body.MessageSid,
-    AccountSid: req.body.AccountSid,
-    message_status: req.body.SmsStatus,
-    communication_recipient_contact: req.body.To,
-    communication_sender_contact: req.body.From
-  };
-  //insert message into the database
-  Queries.insertOneCommunication(message)
-    .then(communication=> {
-      console.log(communication);
-      //insert future logic
-      //res.send(communication);
-      res.writeHead(200, {'Content-Type': 'text/xml'});
-      res.end(twiml.toString());
+  Queries.findStudentandStakeholder(req.body.From)
+    .then(ids => {
+      message= {
+        communication_type_id: 1,
+        raw_body: req.body.Body,
+        timestamp: Date.now(),
+        student_id: ids.student_id,
+        stakeholder_id: ids.stakeholder_id,
+        MessageSid: req.body.MessageSid,
+        AccountSid: req.body.AccountSid,
+        message_status: req.body.SmsStatus,
+        communication_recipient_contact: req.body.To,
+        communication_sender_contact: req.body.From
+      };
+      Queries.insertOneCommunication(message)
+        .then(communication=> {
+          console.log(communication);
+          //insert future logic
+          //res.send(communication);
+          res.writeHead(200, {'Content-Type': 'text/xml'});
+          res.end(twiml.toString());
+        });
     });
-
 });
 
 module.exports = Router;
