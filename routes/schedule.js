9245bd9b4ff6a0af5266646a41cc5a90f43f052f
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-08-09 09:27:15 -0600

    all texts flowing through and adding into local db

---
+++ routes/schedule.js
@@ -0,0 +1,9 @@
+const Express = require('express');
+const Router = Express.Router();
+const Queries = require('../database/schedulequeries');
+
+Router.get('/', (req, res) => {
+
+});
+
+module.exports = Router;
--- twilio/responseHandler.js
+++ twilio/responseHandler.js
@@ -1,24 +1,60 @@
 var texts = require('../twilio/texts.js');
+var Queries = require('../database/schedulequeries');
 
-function getResponse(message){
+function getResponse(message, ids){
+  const {stakeholder_id, student_id} = ids;
+  let availability = [];
+  let days = [];
+  let morning_availability = {
+    stakeholder_id: stakeholder_id,
+    date: '',
+    timeframe: 'morning'
+  };
+  console.log(morning_availability);
+  let afternoon_availability = {
+    stakeholder_id: stakeholder_id,
+    date: '',
+    timeframe: 'afternoon'
+  };
   if(message.toLowerCase().includes('monday') || message.toLowerCase().includes('tuesday') || message.toLowerCase().includes('wednesday') || message.toLowerCase().includes('thursday') || message.toLowerCase().includes('friday')) {
-
     //continue on to update array of blackout dates
     if (message.toLowerCase().includes('monday')) {
-        //schedule.blackoutDates('monday');
+      days.push('monday')
     }
     if (message.toLowerCase().includes('tuesday')) {
-        //schedule.blackoutDates('tuesday');
+      days.push('tuesday')
     }
     if (message.toLowerCase().includes('wednesday')) {
-      //  schedule.blackoutDates('wednesday');
+      days.push('wednesday')
     }
     if (message.toLowerCase().includes('thursday')) {
-      //  schedule.blackoutDates('thursday');
+      days.push('thursday')
     }
     if (message.toLowerCase().includes('friday')) {
-      //  schedule.blackoutDates('friday');
+      days.push('friday')
     }
+    for (var i = 0; i < days.length; i++) {
+      availability.push({
+        stakeholder_id: stakeholder_id,
+        date: days[i],
+        timeframe: 'morning'
+      });
+      availability.push({
+        stakeholder_id: stakeholder_id,
+        date: days[i],
+        timeframe: 'afternoon'
+      });
+    }
+    availability.forEach(period=>{
+      console.log('here');
+      Queries.insertOneDayPeriod(period);
+    })
+
+    // const unique_days = [...new Set(availability.map(item => item.date))];
+    // const unique_days = [...new Set(availability.map(item => item.date))];
+    // console.log(unique_days);
+    // new_days = `'${unique_days.join("', '")}'`;
+    // console.log(new_days);
     //if response looks like day, ask for time of day
     return texts.timeOfDayMessage;
   }else if(message.toLowerCase().includes('morning') || message.toLowerCase().includes('afternoon') || message.toLowerCase().includes('either') || message.toLowerCase().includes('both')){
