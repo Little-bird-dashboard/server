02daea6ef496afc6a74984196c2c185ab4ab6b95
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-08-22 16:48:29 -0600

    adding route to add demo student

--- database/schedulequeries.js
+++ database/schedulequeries.js
@@ -2,7 +2,6 @@ const knex = require('./knex');
 
 module.exports = {
   insertOneDayPeriod: (availability) => {
-    console.log('i was called');
     return knex('guardian_availability').insert(availability).returning('*');
   },
   getGuardianAvailabilityforDoodle: (student_id) => {
