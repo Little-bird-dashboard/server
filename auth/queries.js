73cf609ee620ceabb89247122f3704612042c6a6
Marlena Baker <baker.marlena@gmail.com>
2018-01-14 11:47:04 -0700

    auth changes

--- auth/queries.js
+++ auth/queries.js
@@ -2,7 +2,12 @@ const knex = require('../database/knex');
 
 module.exports = {
   findCoordiantorByEmail: (email) => {
-    return knex('stakeholder').where('email', email).first();
+    return knex('stakeholder as sh')
+      .select('school.name as school', 'login.password as password', 'sh.id as stakeholder_id', 'sh.stakeholder_type_id as stakeholder_type_id', 'st.type as stakeholder_type')
+      .leftJoin('login', 'login.stakeholder_id', 'sh.id')
+      .leftJoin('school', 'login.school_id', 'school.id')
+      .leftJoin('stakeholder_type as st', 'sh.stakeholder_type_id', 'st.id')
+      .where('sh.email', email).first();
   },
   findPasswordById: (id) => {
     return knex('login').select('password').where('stakeholder_id', id).first();
--- auth/routes.js
+++ auth/routes.js
@@ -11,84 +11,39 @@ function validCoordinator(coordinator){
   return validEmail && validPassword;
 }
 
-// router.post('/signup', (req, res, next) => {
-//   if(validCoordinator(req.body)){
-//     //check to see if email is unique
-//     queries
-//       .findCoordiantorByEmail(req.body.email)
-//       .then(coordinator => {
-//         if(coordinator) {
-//           //they were foudn and this email is not unique
-//           next(new Error('Email in use'));
-//         } else {
-//           //not found and should insert
-//           const coordinator = {
-//             first_name: req.body.first_name,
-//             last_name: req.body.last_name,
-//             email: req.body.email,
-//             type: req.body.type,
-//             cell: req.body.cell,
-//
-//           };
-//
-//           bcrypt
-//           .hash(req.body.password, 10)
-//           .then((hash) => {
-//             coordinator.password = hash;
-//             queries
-//               .createCoordinator(coordinator)
-//               .then(coordinator => {
-//                 res.json(coordinator);
-//               });
-//           });
-//
-//         }
-//       });
-//   } else {
-//     next(new Error('Invalid User'));
-//   }
-// });
-
 router.post('/login', (req, res, next) => {
-  console.log('hitting the route')
   if(validCoordinator(req.body)){
     //check to see if email is unique
     queries
       .findCoordiantorByEmail(req.body.email)
       .then(coordinator => {
-        console.log(coordinator)
         if(coordinator) {
-          //compare passwords
-          queries
-            .findPasswordById(coordinator.id)
-            .then(response => {
-              console.log(response.password)
-              bcrypt
-              .compare(req.body.password, response.password)
-              .then((result) => {
-                console.log(result)
-                if(result){
-              queries
-                .findStakeholderTypeById(coordinator.stakeholder_type_id)
-                .then(stakeholerType => {
-                  let token = jwt.sign({id:coordinator.id, type:stakeholerType.type}, 'testKey')
-                  res.json({token:token});
-                })
-            }else {
-              res.sendStatus(403)
-              next(new Error('Incorrect password'));
-            }
-          })
-        })
+          bcrypt
+            .compare(req.body.password, coordinator.password)
+            .then((result) => {
+              if(result){
+                let token = jwt.sign({id:coordinator.id, type:coordinator.stakeholder_type}, 'testKey')
+                let stakeholder = {
+                  token: token,
+                  school: coordinator.school,
+                  type: coordinator.stakeholder_type,
+                  id: coordinator.stakeholder_id
+                }
+                res.json(stakeholder);
+              } else {
+                res.setStatus(403)
+                next(new Error('Incorrect password'));
+              }
+            })
           //they were foumnd and login
         } else {
           //not found should signup
-          res.sendStatus(400)
+          res.setStatus(400)
           next(new Error('Email not found. Please create an account'));
         }
       });
   } else {
-    res.sendStatus(403)
+    res.setStatus(403)
     next(new Error('Invalid User Credentials'));
   }
 });
