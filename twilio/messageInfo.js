f7789daed7bbd558f68736bcd4e1e99eb4f41f5a
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2018-01-21 18:01:32 -0700

    testing auto for json credentials

--- twilio/messageInfo.js
+++ twilio/messageInfo.js
@@ -19,8 +19,8 @@ function prepareMessage(message,data){
   if(data.language_id==2){
     const translate = new Translate({
       projectId: projectId,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      client_email: process.env.GOOGLE_CLIENT_EMAIL
+      // private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      // client_email: process.env.GOOGLE_CLIENT_EMAIL
     });
 
     const text = message.Body;
