099e38fe65e1c28a8dd19e17aaea421ae5d40a95
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2018-01-21 18:25:34 -0700

    trying private key and client email

--- twilio/messageInfo.js
+++ twilio/messageInfo.js
@@ -14,13 +14,14 @@ function storeMessageInfo_incoming(message) {
 }
 
 function prepareMessage(message,data){
+  console.log('preparing');
   let message_body = '';
   let language = 1;
   if(data.language_id==2){
     const translate = new Translate({
       projectId: projectId,
-      // private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      // client_email: process.env.GOOGLE_CLIENT_EMAIL
+       private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+       client_email: process.env.GOOGLE_CLIENT_EMAIL
     });
 
     const text = message.Body;
